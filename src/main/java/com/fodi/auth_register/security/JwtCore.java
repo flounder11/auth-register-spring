605b327a3fec72db383e27cbe0670630b46bacb9
SidereaH <hutornoyaa@gmail.com>
2025-03-26 20:09:43 +0300

    fix error in jwt generating

--- src/main/java/com/fodi/auth_register/security/JwtCore.java
+++ src/main/java/com/fodi/auth_register/security/JwtCore.java
@@ -1,10 +1,12 @@
 package com.fodi.auth_register.security;
 
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import io.jsonwebtoken.*;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
@@ -12,14 +14,19 @@ public class JwtCore {
     @Value("${app.secret}")
     private String secret;
     @Value("${app.lifetime}")
-    private String lifetime;
+    private int lifetime;
 
     public String generateToken(Authentication auth) {
         UserDetailsImpl user = (UserDetailsImpl) auth.getPrincipal();
-        return Jwts.builder().setSubject((user.getUsername())).setIssuedAt(new Date())
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
+
+        return Jwts.builder()
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date())
                 .setExpiration(new Date((new Date()).getTime() + lifetime))
-                .signWith(SignatureAlgorithm.HS512, secret)
+                .signWith(key)
                 .compact();
+
     }
 
     public String getNameFromJwt(String token) {
